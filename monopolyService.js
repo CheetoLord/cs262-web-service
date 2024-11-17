a0424330bfa0ff1d3ea0b019d3583985e461803d
Peter Lund <112506867+CheetoLord@users.noreply.github.com>
2024-11-16 23:13:27 -0500

    Update monopolyService.js app.use AGAIN

--- monopolyService.js
+++ monopolyService.js
@@ -62,7 +62,12 @@ router.get('/playergames/:id', readPlayerGames);
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
 });
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
